d33491ce8fe61ef95630cbc755c7a89b0e4b61ce
Brent Reynolds <brentsreynolds5@gmail.com>
2023-12-30 09:26:43 -0500

    Triangle Exercism exercise

---
+++ Triangle.cs
@@ -0,0 +1,61 @@
+using System;
+
+public static class Triangle
+{
+    public static bool IsScalene(double side1, double side2, double side3)
+    {
+        if (side1 + side2 >= side3 && side2 + side3 >= side1 && side1 + side3 >= side2 && side1 > 0 && side2 > 0 && side3 > 0)
+        {
+            if (side1 != side2 && side2 != side3 && side1 != side3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static bool IsIsosceles(double side1, double side2, double side3)
+    {
+        if (side1 + side2 >= side3 && side2 + side3 >= side1 && side1 + side3 >= side2 && side1 > 0 && side2 > 0 && side3 > 0)
+        {
+            if (side1 == side2 || side1 == side3 || side2 == side3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static bool IsEquilateral(double side1, double side2, double side3)
+    {
+        if (side1 + side2 >= side3 && side2 + side3 >= side1 && side1 + side3 >= side2 && side1 > 0 && side2 > 0 && side3 > 0)
+        {
+            if (side1 == side2 && side2 == side3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
